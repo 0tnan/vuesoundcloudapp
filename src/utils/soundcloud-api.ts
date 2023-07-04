b341be70e8ce7c8745970ca77136573c9f5611f9
Andriamanantoanina RAKOTONDRANALY <andriamanantoanina@rakotondranaly.fr>
2023-07-04 13:23:42 +0200

    playlist direct integration into favorites optimized playlist fetching

--- src/utils/soundcloud-api.ts
+++ src/utils/soundcloud-api.ts
@@ -1,6 +1,8 @@
 import { Favorites } from "@/interfaces/favorites";
 import { User } from "@/interfaces/user";
+import { PlaylistWithTracks } from "@/interfaces/playlist-with-tracks";
 import { CapacitorHttp } from "@capacitor/core";
+import { Track } from "@/interfaces/track";
 
 const API_URL = "https://api-v2.soundcloud.com";
 const soundcloudOptions = {
@@ -18,6 +20,78 @@ export async function getFavorites(
     const options = {
       ...soundcloudOptions,
       url: `${API_URL}/users/${profileId}/likes?client_id=${apiKey}`,
+    };
+    CapacitorHttp.get(options)
+      .then((response) => {
+        const results = response.data;
+        if (results) {
+          resolve(results);
+        } else {
+          reject(results);
+        }
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+}
+
+export async function getPlaylistWithTracks(
+  apiKey: string,
+  playlistId: number
+): Promise<PlaylistWithTracks> {
+  return new Promise((resolve, reject) => {
+    const options = {
+      ...soundcloudOptions,
+      url: `${API_URL}/playlists/${playlistId}?client_id=${apiKey}`,
+    };
+    CapacitorHttp.get(options)
+      .then((response) => {
+        const results = response.data;
+        if (results) {
+          resolve(results);
+        } else {
+          reject(results);
+        }
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+}
+
+export async function getTrack(apiKey: string, id: number): Promise<Track> {
+  return new Promise((resolve, reject) => {
+    const options = {
+      ...soundcloudOptions,
+      url: `${API_URL}/tracks/${id}?client_id=${apiKey}`,
+    };
+    CapacitorHttp.get(options)
+      .then((response) => {
+        const results = response.data;
+        if (results) {
+          resolve(results);
+        } else {
+          reject(results);
+        }
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+}
+
+export async function getMultipleTracks(
+  apiKey: string,
+  ids: number[]
+): Promise<Track[]> {
+  return new Promise((resolve, reject) => {
+    const joinedIds = ids.join(",");
+    const options = {
+      ...soundcloudOptions,
+      url: `${API_URL}/tracks?ids=${encodeURIComponent(
+        joinedIds
+      )}&client_id=${apiKey}`,
     };
     CapacitorHttp.get(options)
       .then((response) => {
